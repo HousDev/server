19469e111b6248d67cb2bd345175644b59b507e7
Sachin Paithane <sachin@hously.in>
2026-02-09 12:20:29 +0530

    working on service order.

--- routes/templateRoutes.js
+++ routes/templateRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {
   downloadProjectTemplate,
   downloadItemsExcelTemplate,
+  downloadServicesExcelTemplate,
 } = require("../controllers/templateController");
 
 /**
@@ -12,5 +13,6 @@ const {
 
 router.get("/project-import", downloadProjectTemplate);
 router.get("/items-import-template", downloadItemsExcelTemplate);
+router.get("/services-import-template", downloadServicesExcelTemplate);
 
 module.exports = router;
